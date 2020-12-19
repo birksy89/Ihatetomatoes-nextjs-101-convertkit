ed46992f0ca62553f0984ec2478b2d5a6af1c0b3
Andrew <andrew@bbm.co.uk>
2020-12-19 12:18:05 +0000

    Serverside POST to ConvertKit

--- src/pages/api/subscribe.js
+++ src/pages/api/subscribe.js
@@ -1,4 +1,4 @@
-export default (req, res) => {
+export default async (req, res) => {
   const { email } = req.query;
   console.log(email);
 
@@ -6,6 +6,30 @@ export default (req, res) => {
     return res.status(400).json({ error: "Email is required" });
   }
 
-  //   res.statusCode = 200;
-  //   res.json({ name: "John Doe" });
+  try {
+    const FORM_ID = process.env.CONVERTKIT_FORM_ID;
+    const API_KEY = process.env.CONVERTKIT_API_KEY;
+    const API_URL = process.env.CONVERTKIT_API_URL;
+
+    // Send request to ConvertKit
+    const data = { email, api_key: API_KEY };
+
+    const response = await fetch(`${API_URL}forms/${FORM_ID}/subscribe`, {
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+    });
+
+    // Any error from CK = return custom message
+    if (response.status >= 400) {
+      return res.status(400).json({
+        error: `There was an error subscribing to the list.`,
+      });
+    }
+    return res.status(201).json({ error: "" });
+  } catch (error) {
+    return res.status(500).json({ error: error.message || error.toString() });
+  }
 };
