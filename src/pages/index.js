bf0c0aa8f5bdf3f2fa2988719dd69e016e549825
Andrew <andrew@bbm.co.uk>
2020-12-19 08:36:20 +0000

    TailWind CSS Clasees

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,22 +3,14 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
+    <div className="mx-auto md:max-w-3xl md:px-4 min-h-screen flex flex-col justify-center">
       <Head>
         <title>Next.js 101</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="min-h-screen flex flex-col justify-center">
-        <img
-          className="m-auto my-4 w-24 sm:w-auto"
-          alt="Ihatetomatoes"
-          src="/assets/img_logo.svg"
-        />
-        <h1 className="mx-auto text-4xl font-bold">Welcome to Next.js 101!</h1>
-        <Link href="/page-two">
-          <a className="text-red-400"> Go to page two</a>
-        </Link>
+      <main className="bg-white p-4">
+        <p>Hero Component</p>
       </main>
     </div>
   );
