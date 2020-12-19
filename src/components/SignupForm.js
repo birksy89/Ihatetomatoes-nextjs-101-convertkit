74ef2a5cace4986e69a248b3836058eaad8cbaac
Andrew <andrew@bbm.co.uk>
2020-12-19 10:16:57 +0000

    Using "ClassNames" conditional CSS Classes

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -13,23 +13,37 @@ const SuccessMessage = () => (
 const SignupForm = ({ title }) => {
   const isLoading = true;
 
+  // css classes for our UI
+  const formClass = classNames({
+    "flex items-center border rounded-md border-gray-300 p-1 focus-within:border-blue-500 focus-within:ring-blue-200 focus-within:ring-4": true,
+    "bg-gray-100 border-gray-100": isLoading,
+  });
+
+  const inputClass = classNames({
+    "appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none": true,
+    "opacity-50 cursor-not-allowed": isLoading,
+  });
+
+  const btnClass = classNames({
+    "flex-shrink-0 bg-red-500 hover:bg-red-600 border-red-500 hover:border-red-600 text-sm border-4 text-white py-1 px-2 rounded": true,
+    "opacity-50 cursor-not-allowed": isLoading,
+  });
+
   return (
     <>
       <p className="p-1 mb-2">{title}</p>
       <SuccessMessage />
       <form className="max-w-sm">
-        <div className="flex items-center border rounded-md border-gray-300 p-1 focus-within:border-blue-500 focus-within:ring-blue-200 focus-within:ring-4">
+        <div className={formClass}>
           <input
-            className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+            className={inputClass}
             type="text"
             placeholder="Jane Doe"
             aria-label="Full name"
+            disabled={isLoading}
           />
-          <button
-            className="flex-shrink-0 bg-red-500 hover:bg-red-600 border-red-500 hover:border-red-600 text-sm border-4 text-white py-1 px-2 rounded"
-            type="submit"
-          >
-            Sign Up
+          <button className={btnClass} disabled={isLoading} type="submit">
+            {isLoading ? "Processing" : "Sign Up"}
           </button>
         </div>
         <ErrorMessage message="Please enter a valid email." />
